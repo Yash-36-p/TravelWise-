a13391e9e17d4a50daccc999509dce2fc6bd58fe
Yash Pandey <yash.pandey5623@gmail.com>
2026-01-12 20:55:10 +0000

    fix case-sensitive model imports for linux

--- backend/routes/budgetRoutes.js
+++ backend/routes/budgetRoutes.js
@@ -1,27 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const Budget = require("../models/Budget");
-
-// // GET budget
-// router.get("/:email", async (req, res) => {
-//   const budget = await Budget.findOne({ userEmail: req.params.email });
-//   res.json(budget);
-// });
-
-// // SET / UPDATE budget
-// router.post("/", async (req, res) => {
-//   const { userEmail, total, spent, remaining } = req.body;
-
-//   const budget = await Budget.findOneAndUpdate(
-//     { userEmail },
-//     { total, spent, remaining },
-//     { upsert: true, new: true }
-//   );
-
-//   res.json(budget);
-// });
-
-// module.exports = router;
 
 const express = require("express");
 const router = express.Router();
--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,16 +1,4 @@
 
-// const express = require("express");
-// const router = express.Router();
-// const {
-//   addExpense,
-//   getExpenses,
-// } = require("../controllers/expenseController");
-
-// router.post("/", addExpense);
-// router.get("/:email", getExpenses);
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 
